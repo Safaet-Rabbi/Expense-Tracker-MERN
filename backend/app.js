b757dfc70a81c084bcc9494ddbba9ec3cd7bcce1
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-08-27 19:53:09 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/expense-tracker');
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
